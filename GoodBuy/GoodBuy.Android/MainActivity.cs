af90316c6f3aa3f82fdae5d9cb552adc5c245e8d
DESKTOP-VK5930F\William Pegler <william-lpa@hotmail.com>
2017-08-19 20:45:25 -0300

    introdução suporte push

--- GoodBuy/GoodBuy.Android/MainActivity.cs
+++ GoodBuy/GoodBuy.Android/MainActivity.cs
@@ -2,20 +2,27 @@
 
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
-using GoodBuy.Service;
-using GoodBuy.Authentication;
-using GoodBuy.Droid;
+using Gcm.Client;
+using goodBuy.Droid;
+using GoodBuy;
 
 [assembly: Xamarin.Forms.Dependency(typeof(MainActivity))]
-namespace GoodBuy.Droid
+namespace goodBuy.Droid
 {
     [Activity(Label = "GoodBuy", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static MainActivity instance = null;
+        // Return the current activity instance.
+        public static MainActivity CurrentActivity
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -29,6 +36,32 @@ namespace GoodBuy.Droid
             SQLitePCL.Batteries.Init();
 
             LoadApplication(new App());
+            instance = this;
+
+            try
+            {
+                // Check to ensure everything's set up right
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+                // Register for push notifications
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
+        }
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
         }
     }
 }
