6a1b324b8dbb5ca3cada0b65ca6e3b5ae2d07524
william-lpa <william-lpa@hotmail.com>
2017-10-05 13:01:10 -0300

    inicio tabbed page.

---
+++ GoodBuy.Core/Views/CompartilharOfertasPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoodBuy.Core.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CompartilharOfertasPage : TabbedPage
+    {
+        public CompartilharOfertasPage()
+        {
+            InitializeComponent();
+            using (var scope = App.Container.BeginLifetimeScope())
+            {
+                var grupoofertaVm = scope.Resolve<ViewModels.GruposOfertasPageViewModel>();
+                grupoofertaVm.NotSharing = false;
+                grupoOfertas.BindingContext = grupoofertaVm;
+            }
+        }
+    }
+}
